c668b83af0834c2d0b2b2b20a970af50def44c20
Bruno Riccardi <br.riccardi@gmail.com>
2017-12-09 09:35:20 +0100

    Initial Commit

---
+++ 08_12/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  08_12
+//
+//  Created by Bruno Riccardi on 09/12/2017.
+//  Copyright © 2017 Bruno Riccardi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
