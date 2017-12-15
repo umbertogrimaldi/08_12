0234b75ca8bfabb6d70a1b0f24070dc8b027eaa3
Umberto <grimaldiumberto94@gmail.com>
2017-12-15 14:52:02 +0100

    class changes

--- 08_12/TableViewCell.swift
+++ 08_12/TableViewCell.swift
@@ -12,16 +12,24 @@ class TableViewCell: UITableViewCell {
 
     @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var myText: UILabel!
-    
+    @IBOutlet weak var labelCategory: UILabel!
     
     func setBook(book : Book)  {
         
          let myImagen = UIImage(named: book.image)
         
         myImage.image = myImagen
-        
         myText.text = book.title
+        labelCategory.text = book.category
         
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        labelCategory.layer.cornerRadius = 5.0
+        labelCategory.layer.borderWidth = 1.0
+        labelCategory.layer.borderColor = UIColor.purple.cgColor
     }
 
 }
--- 08_12/book.swift
+++ 08_12/book.swift
@@ -13,11 +13,15 @@ class Book {
     var image: String
     var title: String
     var text: String
+    var author: String
+    var category: String
     
-    init(image: String,title: String, text: String){
+    init(image: String, title: String, text: String, author: String, category: String){
         self.image = image
         self.title = title
         self.text = text
+        self.author = author
+        self.category = category
     }
     
    
@@ -26,11 +30,11 @@ class Book {
         
         var books: [Book] = []
         
-        let book1 = Book(image: "mikey.jpg", title: "MikeyMouse", text: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.")
+        let book1 = Book(image: "mikey.jpg", title: "MikeyMouse", text: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.", author: "AUTOREUNOOO", category: "CATEGORIAUNOOOOOO")
         
-        let book2 = Book(image: "mutti.jpg", title: "MuttiSauce", text: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.")
+        let book2 = Book(image: "mutti.jpg", title: "MuttiSauce", text: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.", author: "AUTOREUNOOO", category: "CATEGORIAUNOOOOOO")
         
-          let book3 = Book(image: "apple.jpg", title: "apple", text: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.")
+          let book3 = Book(image: "apple.jpg", title: "apple", text: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.", author: "AUTOREUNOOO", category: "CATEGORIAUNOOOOOO")
         
         
         
